eb62d4a682eab1d64f2aa20e4bc109b28d8468c9
nacho21herreria <nacho21h@live.com>
2017-05-22 17:48:20 -0500

    Prueba2

--- Prueba/personas.js
+++ Prueba/personas.js
@@ -1,4 +1,4 @@
-//datos population
+//datos population 
 var personas=[{"year":1850,"age":0,"sex":1,"people":1483789},
 {"year":1850,"age":0,"sex":2,"people":1450376},
 {"year":1850,"age":5,"sex":1,"people":1411067},
