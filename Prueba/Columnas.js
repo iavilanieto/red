0344d76ccd909b449659b93dded3393c272fa6b8
nacho21herreria <nacho21h@live.com>
2017-06-08 11:42:00 -0500

    Prueba manejo dinamico

--- Prueba/Columnas.js
+++ Prueba/Columnas.js
@@ -1,11 +1,11 @@
 function generarColumnas(){
     return {
-  "$schema": "https://vega.github.io/schema/vega/v3.0.json",
-  "width": 900,
-  "height": 500,
-  "autosize": "none",
+  "$schema": esquema,
 
- "data": [
+        "width": personas2.length+500,
+        "height": personas2.length+200,
+        "padding": 5,
+         "data": [
             {
                 "name": "personas",
                 "values": personas2
@@ -24,14 +24,141 @@ function generarColumnas(){
 
   "signals": [
     {
-      "name": "type",
-      "from":{"data":"valores"},
+      "name": "EjeX",
+      "value": "col2",
       "bind": {
         "input": "select",
-        "options": [
-        "col2"]
+        "options": ["col1","col2","col3","col4","col5","col6"]
       }
-    }]
+    },
+    {
+      "name": "EjeY",
+      "value": "col5",
+      "bind": {
+        "input": "select",
+        "options": ["col1","col2","col3","col4","col5","col6"]
+      }
+    },    
+    {
+      "name": "Barras1",
+      "value": "col3",
+      "bind": {
+        "input": "select",
+        "options": ["col1","col2","col3","col4","col5","col6"]
+      }
+    },    
+    {
+      "name": "Barras2",
+      "value": "col4",
+      "bind": {
+        "input": "select",
+        "options": ["col1","col2","col3","col4","col5","col6"]
+      }
+    },
+
+    {
+      "name": "sex", "value": "Todos",
+      "bind": {"input": "radio", "options": ["Hombres", "Mujeres", "Todos"]}
+    }
+    ],
+        "scales": [
+            {
+                "name": "x",
+                "type":  "band",
+                "range": "width",
+                "nice": true,
+                "domain": {"data": "personas", "field": {"signal":"EjeX"}},
+                "padding": 0.1
+            },
+            {
+                "name": "y",
+                "type": "linear",
+                "range": "height",
+                 "round": true,
+                "zero": true,
+                "nice": true,
+                "domain": {"data": "personas", "field": {"signal":"EjeY"}},
+            }
+        ],
+
+        "axes": [
+            {"orient": "bottom", "scale": "x", "zindex": 1},
+            {"orient": "left", "scale": "y", "zindex": 1}
+        ]
+        ,
+
+        "marks": [
+            {
+                "type": "group",
+                "from": {
+                    "facet": {
+                        "data": "personas",
+                        "name": "facet",
+                        "groupby": "col2"
+                    }
+                },
+                "encode": {
+                    "enter": {
+                        "x": {"scale": "x", "field": {"signal":"EjeX"}}
+                    }
+                },
+                "signals": [
+                    {"name": "height", "update": "bandwidth('x')"}
+                ],
+
+                "scales": [
+                    {
+                        "name": "pos",
+                        "type": "band",
+                        "range": "height",
+                        "domain": {"data": "facet", "field": {"signal":"Barras1"}}
+                    }
+                ],
+                "marks": [
+                    {
+                        "name": "bars",
+                        "from": {"data": "facet"},
+                        "type": "rect",
+                        "encode": {
+                               "enter": {
+                                "x": {"scale": "pos","field": {"signal":"Barras1"},"band": 0.5},
+                                "y": {"scale": "y", "field": {"signal":"Barras1"}},
+                                "width": {"scale": "pos", "band": 0.5},
+                                "y2": {"scale": "y", "value": 0},
+                                "fill": {"value": "grey"}
+                            }, 
+                            "update":{
+                                "x": {"scale": "pos","field": {"signal":"Barras1"},"band": 0.5},
+                                "y": {"scale": "y", "field": {"signal":"Barras1"}},
+                                "width": {"scale": "pos", "band": 0.5},
+                                "y2": {"scale": "y", "value": 0},
+                        }
+                      }
+                    },
+                    {
+                        "name": "bars1",
+                        "from": {"data": "facet"},
+                        "type": "rect",
+                        "encode": {
+                            "enter": {
+                                "x": {"scale": "pos","field": {"signal":"Barras2"},"band": 0},
+                                "y": {"scale": "y", "field": {"signal":"Barras2"}},
+                                "width": {"scale": "pos", "band": 0.5},
+                                "y2": {"scale": "y", "value": 0},
+                                "fill": {"value": "#659CCA"}
+                            }, 
+                            "update":{
+                                "x": {"scale": "pos","field": {"signal":"Barras2"},"band": 0},
+                                "y": {"scale": "y", "field": {"signal":"Barras2"}},
+                                "width": {"scale": "pos", "band": 0.5},
+                                "y2": {"scale": "y", "value": 0},
+                        }
+                      }
+                    }]
+                  }
+            ]
+
+
 
 
 
